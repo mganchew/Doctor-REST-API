9c5bf12eaaf5493db9e56fec872ee8860eac4a62
Mladen Ganchev <mganchev@sevone.com>
2015-09-12 10:45:31 +0300

    fileUpload

---
+++ site/fileTransfer.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+
+<div>
+    <form method="post" enctype="multipart/form-data" action="test.php">
+
+        <input type="file" name="uploadFile"><br>
+        <button type="submit" name="submit" value="submit">Button</button>
+
+    </form>
+</div>
+
+</body>
+</html>
---
+++ site/pdf.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+<div align="center">
+    <embed src="uploads/150610 Mavenir UAG Performance Counters Reference 4.5 v3.pdf" width="1000px" height="2100px">
+</div>
+</body>
+</html>
