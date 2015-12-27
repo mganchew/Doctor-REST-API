32bcbd1f0330f4176e11b0fa2395b486721b3038
mganchew <m.ganchew@gmail.com>
2015-12-27 12:06:20 +0200

    fix

--- site/view/uploadFileForm.php
+++ site/view/uploadFileForm.php
@@ -5,7 +5,7 @@ require 'header.php';
 <div>
     <form method="post" enctype="multipart/form-data" action="upload.php">
 
-        <input type="file" name="uploadFile"><br>
+        Моля качете вашият медицински картон <input type="file" name="uploadFile"><br>
         <button type="submit" name="submit" value="submit">Button</button>
 
     </form>
