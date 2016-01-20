f24e0e7c796985149c50a068708b656264c3209c
Mladen Ganchev <mganchev@sevone.com>
2016-01-20 13:51:41 +0200

    TODO: fix uploadFileForm Session!!!!

---
+++ CurlGoogleFit.php
@@ -0,0 +1,116 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: mladen
+ * Date: 15-7-14
+ * Time: 10:54
+ */
+class CurlGoogleFit
+{
+
+    protected $uri;
+    protected $url;
+    protected $data;
+    protected $rawData;
+    protected $postData;
+    protected $getMethod;
+
+
+    public function setPostData($postData)
+    {
+
+        $this->postData = $postData;
+
+    }
+
+    public function setMethod()
+    {
+
+
+    }
+
+    public function getPostData()
+    {
+
+        return $this->postData;
+
+    }
+
+    public function setUrl($url)
+    {
+
+        $this->url = $url;
+
+    }
+
+    public function getUrl()
+    {
+
+        return $this->url;
+
+    }
+
+    public function getResponse()
+    {
+
+        $credentials = 'ya29.bwJyJFx8IrwKg9t_NtjD9kiWsmaUXPpKSTKRUdP9FydDHZlkNlWMYH8u8wlNgvbURGzg';
+        $headers = array(
+
+            "Authorization: Bearer $credentials",
+            "Content-Type: application/json;encoding=utf-8"
+
+        );
+
+        $handle = curl_init();
+        curl_setopt($handle, CURLOPT_URL, $this->getUrl());
+        curl_setopt($handle, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($handle, CURLOPT_RETURNTRANSFER, TRUE);
+        if (isset($this->postData)) {
+            curl_setopt($handle, CURLOPT_CUSTOMREQUEST, 'PATCH');
+            curl_setopt($handle, CURLOPT_POSTFIELDS, $this->getPostData());
+        }else{
+            curl_setopt($handle, CURLOPT_CUSTOMREQUEST, 'GET');
+        }
+        curl_setopt($handle, CURLOPT_SSL_VERIFYHOST, FALSE);
+        curl_setopt($handle, CURLOPT_SSL_VERIFYPEER, FALSE);
+
+        $response = curl_exec($handle);
+        curl_close($handle);
+        return $response;
+
+    }
+
+}
+
+$response = new CurlGoogleFit();
+$response->setUrl('https://www.googleapis.com/fitness/v1/users/me/dataSources/derived:com.example.myapp.mycustomtype:319250874787:Example%20Manufacturer:ExampleTablet:1000002:hearthbeat123/datasets/1453282751200000000-1453282751243623929');
+$data = '{
+        "dataSourceId": "derived:com.example.myapp.mycustomtype:319250874787:Example Manufacturer:ExampleTablet:1000002:hearthbeat123",
+        "maxEndTimeNs": 1397515179728708316,
+        "minStartTimeNs": 1397513334728708316,
+        "point": [
+            {
+                "dataTypeName": "com.example.myapp.mycustomtype",
+                "endTimeNanos": 1397513365565713993,
+                "originDataSourceId": "",
+                "startTimeNanos": 1397513334728708316,
+                "value": [
+                    {
+                        "intVal": 11
+                    }
+                ]
+            }
+        ]
+    }';
+//var_dump(json_decode($data));
+//$response->setPostData($data);
+//$response->setUrl('https://www.googleapis.com/fitness/v1/users/me/dataSources');
+$data1 = json_decode($response->getResponse());
+//var_dump($data1->point[0]->value[0]->intVal);
+//foreach ($data1 as $key => $value) {
+//
+//
+//
+//}
+var_dump(json_decode($response->getResponse()));
--- site/js/googleAPI.js
+++ site/js/googleAPI.js
@@ -53,6 +53,7 @@ function createDataSource(data) {
 
 function getUserDataSource(data) {
     token = $("#accessToken").val();
+    var heartbeat = false;
     //console.log(token);
     $.ajax({
         type: 'GET',
@@ -64,19 +65,86 @@ function getUserDataSource(data) {
         },
         dataType: 'json',
         success: function (data) {
+            $.each(data, function (key, value) {
+                //console.log('data');
+                $.each(value, function (key1, value1) {
+                    if (value1.dataStreamName == 'hearthbeat123') {
+                        heartbeat = true;
+                    }
+                });
+            });
+            if (!heartbeat) {
+                //console.log("empty");
+                createDataSource();
+            } else {
 
-             if (jQuery.isEmptyObject(data)) {
+                $.each(data, function (key, value) {
+                    //console.log('data');
+                    $.each(value, function (key1, value1) {
+                        console.log(value1);
+                        if (value1.dataStreamName == 'hearthbeat123') {
+                            console.log(value1);
+                            console.log(value1.dataStreamId);
+                            populateDataSource(value1.dataStreamId)
+                        }
+                    });
+                });
 
-            console.log("empty");
-            createDataSource();
-             } else {
-            console.log(data);
+                //console.log(data);
             }
         },
         error: function () {
             console.log('error');
         }
     });
+}
+
+function populateDataSource(dataSourceId) {
+
+    dataTypeName = dataSourceId.split(':')[1];
+    console.log(dataTypeName);
+
+    var body = {
+        "dataSourceId": dataSourceId,
+        "maxEndTimeNs": 1397515179728708316,
+        "minStartTimeNs": 1397513334728708316,
+        "point": [
+            {
+                "dataTypeName": dataTypeName,
+                "endTimeNanos": 1397513365565713993,
+                "originDataSourceId": "",
+                "startTimeNanos": 1397513334728708316,
+                "value": [
+                    {
+                        "intVal": 10
+                    }
+                ]
+            }
+        ]
+    };
+    //TODO $nanotime = system('date +%s%N'); in php script
+    token = $("#accessToken").val();
+
+    $.ajax({
+        type: 'GET',
+        url: "https://www.googleapis.com/fitness/v1/users/me/dataSources/" + dataSourceId + "/datasets/1453282751243623925-1453282751243623925",
+        data: JSON.stringify(body),
+        headers: {
+            "Authorization": "Bearer " + token,
+            "Content-Type": "application/json;encoding=utf-8"
+        },
+        dataType: 'json',
+        success: function (data) {
+            console.log(data);
+            //console.log(data.point[0].value[0].intVal);
+        },
+        error: function () {
+            console.log('error');
+        }
+    });
+
+
+
 }
 
 $(document).ready(function () {
